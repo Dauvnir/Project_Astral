bc166b61436acba0277d4259194135858286800a
Dauvnir <134309790+Dauvnir@users.noreply.github.com>
2023-11-23 00:33:15 +0100

    Revert "WelcomePage - done"
    This reverts commit d8699b4fef81b7b07de86f8e24a496c36d1dfd6e.

--- Astral/src/App.jsx
+++ Astral/src/App.jsx
@@ -1,28 +1,6 @@
 import Background from './components/Background';
-<<<<<<< Updated upstream
 function App() {
 	return <Background></Background>;
 }
-=======
-import ImgCarouselWrapper from './components/ImgCarouselWrapper';
-import LogoHeader from './components/LogoHeader';
-import WelcomeMsg from './components/WelcomeMsg';
-import JoinMsg from './components/JoinMsg';
-import LibraryBtn from './components/LibraryBtn';
-import {Indicator} from './components/Indicator';
-const App = () => {
-	return (
-		<>
-			<Background></Background>
-			<LogoHeader></LogoHeader>
-			<WelcomeMsg></WelcomeMsg>
-			<ImgCarouselWrapper></ImgCarouselWrapper>
-			<JoinMsg></JoinMsg>
-			<LibraryBtn></LibraryBtn>
-			<Indicator></Indicator>
-		</>
-	);
-};
->>>>>>> Stashed changes
 
 export default App;
