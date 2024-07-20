b9455dec53dfaaf6ded4c8edf45d2c1eab42d1b4
Dauvnir <134309790+Dauvnir@users.noreply.github.com>
2024-07-20 14:54:46 +0200

    added babel

--- Astral/vite.config.js
+++ Astral/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import pluginRewriteAll from "vite-plugin-rewrite-all"; // <= import the plugin
+import babel from "vite-plugin-babel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		react(),
 		pluginRewriteAll(),
+		babel(),
 		react({
 			include: /\.(jsx|tsx)$/,
 			babel: {
