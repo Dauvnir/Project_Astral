279779ba9df8f9536af3274cf0c01c8a9bf9872a
Dauvnir <134309790+Dauvnir@users.noreply.github.com>
2024-07-20 15:08:27 +0200

    styled component plugin

--- Astral/vite.config.js
+++ Astral/vite.config.js
@@ -1,21 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import pluginRewriteAll from "vite-plugin-rewrite-all"; // <= import the plugin
-import babel from "vite-plugin-babel";
-
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		pluginRewriteAll(),
-		babel(),
-		react({
-			include: /\.(jsx|tsx)$/,
-			babel: {
-				plugins: ["styled-components"],
-				babelrc: false,
-				configFile: false,
-			},
-		}),
-	],
+	plugins: [react(), pluginRewriteAll()],
 });
