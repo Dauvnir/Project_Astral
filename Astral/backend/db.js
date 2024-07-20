526761d4739d4e4afc0fe3a44d5546f1cb90ab0f
Dauvnir <134309790+Dauvnir@users.noreply.github.com>
2024-07-20 14:35:03 +0200

    Database connect.

--- Astral/backend/db.js
+++ Astral/backend/db.js
@@ -2,9 +2,9 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-	user: 'dauvnir',
-	password: 'dauvnir',
-	host: 'localhost',
+	user: "samedi",
+	password: "samedi",
+	host: "localhost",
 	port: 5432,
 	database: 'manhwa_list',
 });
